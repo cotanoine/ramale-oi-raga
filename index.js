a4a5f2076021ee600f0bf830b0ae4f41e653c099
Maillard Antoine <tan.maillard@gmail.com>
2026-03-18 09:25:45 +0100

    Work is now done server_side

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import { randomUUID } from 'node:crypto';
 
 const app = express();
 const server = createServer(app);
@@ -15,21 +16,70 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'));
 });
 
+var cercles = []
 
+// Currently hardcoded in index.html...
+const map_width = 600;
+const map_height = 400;
+
+
+function gaussianRandom(mean=0, stdev=1) {
+    const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+    const v = Math.random();
+    const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    // Transform to the desired mean and standard deviation:
+    return z * stdev + mean;
+}
+
+let letters = "0123456789ABCDEF";
+function random_hex_color() {
+  let color = '#';
+  // Generating 6 times as HTML color code 
+  // consist of 6 letter or digits
+  for (let i = 0; i < 6; i++)
+      color += letters[(Math.floor(Math.random() * 16))];
+  return color;        
+}
 
 io.on('connection', (socket) => {
+
+  let id = randomUUID();
   console.log('a user connected');
-  socket
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('nouveau cercle', (msg) => {
-    console.log('message: ' + msg);
+
+  socket.on('nouveau cercle', (x,y) => {
+    console.log('Nouveau cercle: ' + x + ", "+y);
+    let cercle = {x:x, y:y, dx:gaussianRandom(0,5), dy:gaussianRandom(0,5),rayon:gaussianRandom(40,5), couleur:random_hex_color(), owner: id};
+    cercles.push(cercle);
+    cercles.sort((a,b) => a.rayon - b.rayon);
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+
+  socket.on('move left', (id) => {
+
+  })
 });
+
+
+
+setInterval(() => {
+    cercles.forEach(cercle => {
+    cercle.x += cercle.dx;
+    cercle.y += cercle.dy;
+    if(cercle.x - cercle.rayon < 0 || cercle.x+cercle.rayon > map_width){
+        cercle.dx = - cercle.dx;
+    }
+    if(cercle.y - cercle.rayon < 0 || cercle.y+cercle.rayon > map_height){
+        cercle.dy = - cercle.dy;
+    }     
+  });
+    io.emit('positions', cercles);
+}, 25);
+
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
+
+
