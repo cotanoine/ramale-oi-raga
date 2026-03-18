5634c46efbad070bafae2fb9119da1c1a5c69cd2
cbuvot <>
2026-03-18 11:44:03 +0100

    Eatable orbs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var cercles = []
 
 // Currently hardcoded in index.html...
 const map_width = 1000;
-const map_height = 700;
+const map_height = 1000;
 
 
 function gaussianRandom(mean=0, stdev=1) {
@@ -32,6 +32,9 @@ function gaussianRandom(mean=0, stdev=1) {
     // Transform to the desired mean and standard deviation:
     return z * stdev + mean;
 }
+
+
+
 
 let letters = "0123456789ABCDEF";
 function random_hex_color() {
@@ -45,7 +48,8 @@ function random_hex_color() {
 
 io.on('connection', (socket) => {
 
-  players[socket.id] = { x: 300, y: 200, rayon: 20 };
+  players[socket.id] = { x: 300, y: 200, rayon: 20, couleur: random_hex_color() };
+  socket.emit('your:id', socket.id);
 
   let id = randomUUID();
   console.log('a user connected');
@@ -62,20 +66,31 @@ io.on('connection', (socket) => {
     cercles.sort((a,b) => a.rayon - b.rayon);
   });
 
-  socket.on('player:move', (data) => {  
-        players[socket.id] = { x: data.x, y: data.y, rayon: data.rayon };
-        socket.broadcast.emit('players:update', players);
-  });
+  socket.on('player:move', (data) => {
+    players[socket.id].mouse = { x: data.x, y: data.y };
+});
 
   socket.on('move left', (id) => {
 
   })
 });
 
+const NB_ORBES = 100;
+
+function creerOrbe() {
+    return {
+        x: Math.random() * map_width,
+        y: Math.random() * map_height,
+        rayon: 3,
+        couleur: random_hex_color()
+    };
+}
+
+var orbes = Array.from({ length: NB_ORBES }, creerOrbe);
+
 function distance (a,b){
   return Math.sqrt((a.y - b.y)**2 + (a.x - b.x)**2)
 }
-
 setInterval(() => {
   let num_cercle = 1;
   let number_of_circles = cercles.length;
@@ -94,23 +109,54 @@ setInterval(() => {
     if( cercle.y+cercle.rayon > map_height){
         cercle.dy = - Math.abs(cercle.dy)
     }
-
-    for(let i = num_cercle;i<number_of_circles;i++){
+      orbes.forEach(orbe => {
+        if(distance(orbe, cercle) + orbe.rayon < cercle.rayon + 0.5*orbe.rayon){
+          orbes.splice(orbes.indexOf(orbe),1);
+          cercle.rayon += orbe.rayon/5;
+          
+        }
+      })
+      
+      for(let i = num_cercle;i<number_of_circles;i++){
       let autre_cercle = cercles[i];
       if(distance(cercle, autre_cercle) + cercle.rayon < autre_cercle.rayon + 0.5*cercle.rayon){
         cercles.splice(num_cercle-1,1);
-        autre_cercle.rayon += cercle.rayon/3;
+        autre_cercle.rayon += cercle.rayon/20;
         number_of_circles -= 1
         break
       }
-    }
-    num_cercle += 1;
-  });
+      }
+      num_cercle += 1;
+    });
 
+    // Mise à jour de la position de chaque joueur vers sa souris
+    Object.values(players).forEach(player => {
+        if (!player.mouse) return;
 
+        const dx = player.mouse.x - player.x;
+        const dy = player.mouse.y - player.y;
+        const distance = Math.hypot(dx, dy);
 
+        if (distance > 1) {
+            const vitesse = 3;
+            player.x += (dx / distance) * vitesse;
+            player.y += (dy / distance) * vitesse;
+        }
+    });
+
+    // Envoi des positions à tous les clients
     io.emit('positions', cercles);
-}, 25);
+    io.emit('orbes', orbes);
+    const sortedPlayers = Object.entries(players)
+                                .sort(([, a], [, b]) => a.rayon - b.rayon)
+                                .reduce((obj, [id, player]) => {
+                                  obj[id] = player;
+                              return obj;
+    }, {});
+
+    io.emit('players:update', sortedPlayers);
+
+    }, 25);
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
