1672b2f75b372b60695e370d2965f365fc88b925
Maillard Antoine <tan.maillard@gmail.com>
2026-03-31 22:54:59 +0200

    Removed dead code

--- index.js
+++ index.js
@@ -24,12 +24,6 @@ const map_width  = 5000;
 const map_height = 5000;
 
 // ── Utilitaires ──
-function gaussianRandom(mean=0, stdev=1) {
-    const u = 1 - Math.random();
-    const v = Math.random();
-    const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-    return z * stdev + mean;
-}
 
 let letters = "0123456789ABCDEF";
 function random_hex_color() {
